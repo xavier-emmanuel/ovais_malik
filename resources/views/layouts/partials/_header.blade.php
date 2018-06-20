d19334992e039e3df4029121cec6971591bc0707
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2018-06-20 12:53:05 +0800

    Favicon
    Fixed reference.

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="icon" href="assets/img/favicon.ico" sizes="16x16">
+  <link rel="icon" href="{{ asset('/img/favicon.ico') }}" sizes="16x16">
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="{{ asset('/plugins/bootstrap/bootstrap.min.css') }}">
