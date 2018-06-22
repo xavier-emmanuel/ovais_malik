83188abbae573582d01e7641484bc5ca867918ba
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2018-06-22 21:39:02 +0800

    SASS
    Added SASS files to resources folder under assets > sass subfolder.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/main.scss', 'public/css/main.min.css')
+  .sass('resources/assets/sass/**.scss', 'public/css/main.min.css');
