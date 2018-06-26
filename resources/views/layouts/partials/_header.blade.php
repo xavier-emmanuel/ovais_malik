0e9e13f31c80054e7678804509260d936a407ad5
Jordan <moy27pogi@gmail.com>
2018-06-26 16:57:20 +0800

    Fix bugs in pages

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -21,12 +21,16 @@
 
   <title>Ovais Malik Voiceover | Multilingual Voice Talent - {{ $page }}</title>
 
-  <link rel="canonical" href="{{ url()->full() }}">
-  <meta property="og:url"             content="{{ url()->full() }}" />
-  <meta property="og:type"            content="article" />
-  <meta property="og:title"           content="Ovais Malik Voiceover | Multilingual Voice Talent - {{ $page }}" />
-  <meta property="og:description"     content="{!! $data->content !!}" />
-  <meta property="og:image"           content="{{ url('/public/'.$data->image) }}" />
-  <meta property="og:image:width"     content="1200" />
-  <meta property="og:image:height"    content="630" />
+  @if(Request::is('blogs/*'))
+    <link rel="canonical" href="{{ url()->full() }}">
+    <meta property="og:url"             content="{{ url()->full() }}" />
+    <meta property="og:type"            content="article" />
+    <meta property="og:title"           content="Ovais Malik Voiceover | Multilingual Voice Talent - {{ $page }}" />
+    <meta property="og:description"     content="{!! $data->content !!}" />
+    <meta property="og:image"           content="{{ url('/public/'.$data->image) }}" />
+    <meta property="og:image:width"     content="1200" />
+    <meta property="og:image:height"    content="630" />
+  @else
+ 
+  @endif
 </head>
