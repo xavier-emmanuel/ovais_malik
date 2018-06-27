3958479e5cd85e765ec3acbd30482bc79dcc070b
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2018-06-27 14:11:49 +0800

    Google Plus Script
    Added missing google plus script for blog sharing.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,13 @@
 		js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0&appId=216062112545276&autoLogAppEvents=1';
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));</script>
+	<script>
+		window.___gcfg = {
+			lang: 'en-US',
+			parsetags: 'onload'
+		};
+	</script>
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
 
 	@include('layouts.partials._navbar')
 
