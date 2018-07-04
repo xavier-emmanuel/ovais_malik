2f658aef1bcb8d4e9cac19a14d6650217e592159
Jordan <moy27pogi@gmail.com>
2018-07-04 16:53:18 +0800

    Latest post

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -58,7 +58,7 @@ class PagesController extends Controller
         $blog = Blog::where('slug', $slug)->first();
         $tags = $blog->tags;
         $tagsArray = explode(',', $tags);
-        $latest_post = Blog::orderBy('created_at','ASC')->get();
+        $latest_post = Blog::orderBy('created_at','ASC')->take(8)->get();
         $related_post = Blog::where('category_id', $blog->category_id)->take(6)->skip(1)->get();
         return view('blog_single')->with(array('page' => $slug, 'data' => $blog, 'tags' => $tagsArray, 'latest_post' => $latest_post, 'related_post' => $related_post));
     }
